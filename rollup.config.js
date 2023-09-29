4ea04d3d8ce62de702d103c7697f3995a7691cf7
Jan Zak <zj@zakjan.cz>
2023-09-29 11:50:36 +0200

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import visualizer from 'rollup-plugin-visualizer';
+import terser from '@rollup/plugin-terser';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 function bundle(format, filename, options = {}) {
   return {
