d2a3cb95aaa7d5168b5836662af31242f3605c5c
Davey Hughes <davidralphhughes@gmail.com>
2023-12-30 17:20:36 -0800

    Fix constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,20 @@
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
 
-export const { cursors, geojsonTypes, events, meta, activeStates } = MapboxDraw.constants;
+export const { cursors, geojsonTypes, events, meta, activeStates } =
+  MapboxDraw.constants;
 
 export const modes = {
   ...MapboxDraw.modes,
-  DRAW_CIRCLE: 'draw_circle'
+  DRAW_CIRCLE: "draw_circle",
+  DRAW_LINE_STRING: "draw_line_string",
+  DRAW_POLYGON: "draw_polygon",
+  DRAW_POINT: "draw_point",
+  SIMPLE_SELECT: "simple_select",
+  DIRECT_SELECT: "direct_select",
+  STATIC: "static",
 };
 
 export const properties = {
-  CIRCLE_RADIUS: 'circleRadius',
-  CIRCLE_HANDLE_BEARING: 'circleHandleBearing'
+  CIRCLE_RADIUS: "circleRadius",
+  CIRCLE_HANDLE_BEARING: "circleHandleBearing",
 };
